b7b49995fe3d20869ee45eb1252d043115f514d8
Fabian <accounts@fabianbalsiger.ch>
2017-05-24 11:39:20 +0200

    feat: implement CSV- and ConsoleEvaluatorWriter

--- evaluation/evaluator.py
+++ evaluation/evaluator.py
@@ -8,43 +8,73 @@ class Evaluator:
     """
     Represents a metric evaluator.
     """
+
     def __init__(self, writer: IEvaluatorWriter):
         """
         Initializes a new instance of the Evaluator class.
         :param writer: A writer.
+        :type writer: IEvaluatorWriter
         """
 
         self.metrics = []
-        self.results = []
         self.labels = [0, 1]
         self.label_str = ["Background", "Target"]
         self.writers = [writer]
+        self.is_header_written = False
 
-    def evaluate(self, image: sitk.Image, ground_truth: sitk.Image, write=True):
+    def add_metric(self, metric: IMetric):
+        """
+        Adds a metric to the evaluation.
+        :param metric: The metric.
+        :type metric: IMetric
+        """
+
+        self.metrics.append(metric)
+
+    def add_writer(self, writer: IEvaluatorWriter):
+        """
+        Adds a writer to the evaluation.
+        :param writer: The writer.
+        :type writer: IEvaluatorWriter
+        """
+
+        self.writers.append(writer)
+        self.is_header_written = False  # header changed
+
+    def evaluate(self, image: sitk.Image, ground_truth: sitk.Image):
         """
         Evaluates the metrics on the provided image and ground truth image.
         :param image: The image.
         :param ground_truth: The ground truth image.
-        :param write: Determines whether to write the 
         """
+
+        if not self.is_header_written:
+            self.write_header()
 
         image_arr = sitk.GetArrayFromImage(image)
         ground_truth_arr = sitk.GetArrayFromImage(ground_truth)
 
-        overlap = sitk.LabelOverlapMeasuresImageFilter()
+        confusion_matrix = ConfusionMatrix()  # calculate the confusion matrix
 
-        self.results = []  # clear results
+        results = []  # clear results
 
         # calculate the metrics
         for param_index, metric in enumerate(self.metrics):
             if isinstance(metric, IConfusionMatrixMetric):
-                metric.confusion_matrix = None
+                metric.confusion_matrix = confusion_matrix
 
-            self.results.append(metric.calculate(image_arr, ground_truth_arr))
+            results.append(metric.calculate(image_arr, ground_truth_arr))
 
         # write the results
         for writer in self.writers:
-            writer.write()
+            writer.write(results)
 
-    def add_metric(self, metric: IMetric):
-        self.metrics.append(metric)
+    def write_header(self):
+
+        header = ["ID", "LABEL"]
+
+        for metric in self.metrics:
+            pass
+
+        for writer in self.writers:
+            writer.write_header(header)
--- evaluation/evaluator_writer.py
+++ evaluation/evaluator_writer.py
@@ -1,20 +1,28 @@
 import csv
+import os
 from abc import ABCMeta, abstractmethod
 
 
 class IEvaluatorWriter(metaclass=ABCMeta):
     """
-    Represents an evaluator writer interface, which enables to write or ensemble evaluation results.
+    Represents an evaluator writer interface, which enables to write evaluation results.
     """
 
-    def __init__(self):
-        self.precision = 2
-        self.path = None
-
     @abstractmethod
-    def write(self):
+    def write(self, data: list):
         """
         Writes the evaluation results.
+        :param data: The evaluation data.
+        :type data: list
+        """
+        raise NotImplementedError
+
+    @abstractmethod
+    def write_header(self, header: list):
+        """
+        Writes the evaluation header.
+        :param header: The evaluation header.
+        :type header: list
         """
         raise NotImplementedError
 
@@ -24,13 +32,90 @@ class CSVEvaluatorWriter(IEvaluatorWriter):
     Represents a CSV evaluator writer.
     """
 
-    def write(self):
-        with open(self.path, 'w', newline='') as file:
-            writer = csv.writer(file, delimiter=';', quotechar='|', quoting=csv.QUOTE_MINIMAL)
-            writer.writerow(["IMAGE", "DICE"])
+    def __init__(self, path: str):
+        """
+        Initializes a new instance of the CSVEvaluatorWriter class.
+        :param path: The file path.
+        :type path: str
+        """
+        super().__init__()
+
+        self.path = path
+
+        # check file extension
+        if not self.path.lower().endswith(".csv"):
+            self.path = os.path.join(self.path, ".csv")
+
+        open(self.path, 'w', newline='')  # creates (and overrides an existing) file
+
+    def write(self, data: list):
+        """
+        Writes the evaluation results.
+        :param data: The evaluation data.
+        :type data: list
+        """
+
+        self.write_line(data)
+
+    def write_header(self, header: list):
+        """
+        Writes the evaluation header.
+        :param header: The evaluation header.
+        :type header: list
+        """
+
+        self.write_line(header)
+
+    def write_line(self, data: list):
+        """
+        Writes a line.
+        :param data: The data.
+        :type data: list
+        """
+        with open(self.path, 'a', newline='') as file:
+            writer = csv.writer(file, delimiter=';')
+            writer.writerow(data)
 
 
 class ConsoleEvaluatorWriter(IEvaluatorWriter):
+    """
+    Represents a console evaluator writer.
+    """
 
-    def write(self):
-        pass
+    def __init__(self, precision: int=3):
+        """
+        Initializes a new instance of the ConsoleEvaluatorWriter class.
+        :param precision: The float precision.
+        :type precision: int
+        """
+        super().__init__()
+
+        self.header = None
+        self.precision = precision
+
+    def write(self, data: list):
+        """
+        Writes the evaluation results.
+        :param data: The evaluation data.
+        :type data: list of floats
+        """
+
+        # format all floats with given precision to str
+        data = [val if isinstance(val, str) else "{0:.{1}f}".format(val, self.precision) for val in data]
+
+        # get length for output alignment
+        length = max(len(max(data, key=len)), len(max(self.header, key=len))) + 2
+        print(length)
+        print(",".join("{0:>{1}}".format(val, length) for val in self.header),
+              "\n",
+              ",".join("{0:>{1}}".format(val, length) for val in data),
+              sep='')
+
+    def write_header(self, header: list):
+        """
+        Writes the evaluation header.
+        :param header: The evaluation header.
+        :type header: list of str
+        """
+
+        self.header = header
