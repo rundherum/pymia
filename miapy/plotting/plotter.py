4c5eb5f69520b0b765a7dab32e6556f752761856
Fabian <accounts@fabianbalsiger.ch>
2017-07-26 14:39:45 +0200

    feat(plot_histogram_overlay): various configuration options

--- miapy/plotting/plotter.py
+++ miapy/plotting/plotter.py
@@ -40,20 +40,28 @@ def plot_histogram(path: str, image: sitk.Image, no_bins: int=255, slice_no: int
     plt.close()
 
 
-def plot_histogram_overlay(path: str, image1: sitk.Image, image2: sitk.Image, no_bins: int=255, slice_no: int=-1,
-                           title: str='', xlabel: str='', ylabel: str='') -> None:
+def plot_histogram_overlay(path: str, image1: sitk.Image, image2: sitk.Image,
+                           legend1: str='', legend2: str='',
+                           no_bins: int=255, slice_no: int=-1,
+                           title: str='', xlabel: str='', ylabel: str='',
+                           xaxis_log: bool=False, yaxis_log: bool=False) -> None:
     """Plots a histogram of an image.
 
     Plots either the histogram of a slice of the image or of the whole image.
+    
     Args:
         path (str): The file path.
         image1 (SimpleITK.Image): The first image.
         image2 (SimpleITK.Image): The second image.
+        legend1 (str): The legend for the first image.
+        legend2 (str): The legend for the second image.2
         no_bins (int): The number of histogram bins.
         slice_no (int): The slice number or -1 to take the whole image.
         title (str): The histogram's title.
         xlabel (str): The histogram's x-axis label.
         ylabel (str): The histogram's y-axis label.
+        xaxis_log (bool): Logarithmic scale for x-axis.
+        yaxis_log (bool): Logarithmic scale for y-axis.
 
     Returns:
         None.
@@ -68,8 +76,11 @@ def plot_histogram_overlay(path: str, image1: sitk.Image, image2: sitk.Image, no
     data1 = data1.flatten()
     data2 = data2.flatten()
 
-    plt.hist(data1, bins=no_bins, alpha=0.5)
-    plt.hist(data2, bins=no_bins, alpha=0.5)
+    plt.hist(data1, bins=no_bins, alpha=0.5, label=legend1)
+    plt.hist(data2, bins=no_bins, alpha=0.5, label=legend2)
+    if legend1 or legend2: plt.legend()
+    if xaxis_log: plt.xscale('log')
+    if yaxis_log: plt.yscale('log')
     if title: plt.title(title)
     if xlabel: plt.xlabel(xlabel)
     if ylabel: plt.ylabel(ylabel)
