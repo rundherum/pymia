12b161a281f7c9085cab6afa1e28bbe34e79e8f0
Fabian <accounts@fabianbalsiger.ch>
2017-08-31 14:12:27 +0200

    fix: reference to napoleon

--- docs/conf.py
+++ docs/conf.py
@@ -40,7 +40,7 @@ extensions = ['sphinx.ext.autodoc',
               'sphinx.ext.imgmath',
               'sphinx.ext.todo',
               'sphinx.ext.viewcode',
-              'sphinxcontrib.napoleon']
+              'sphinx.ext.napoleon']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
