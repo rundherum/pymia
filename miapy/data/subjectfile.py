c0f24681bbec2f536d0a5ab4eaa7caa22e01554d
alain <alain.benninger@hotmail.com>
2018-01-09 14:01:19 +0100

    fix(data): remove default sequence_files argument for SubjectFile

--- miapy/data/subjectfile.py
+++ miapy/data/subjectfile.py
@@ -19,7 +19,7 @@ class FileCollection(metaclass=abc.ABCMeta):
 
 class SubjectFile(FileCollection):
 
-    def __init__(self, subject: str, sequence_files: dict=None, gt_files: dict=None) -> None:
+    def __init__(self, subject: str, sequence_files: dict, gt_files: dict=None) -> None:
         self.subject = subject
         self.sequence_files = sequence_files
         self.gt_files = gt_files
