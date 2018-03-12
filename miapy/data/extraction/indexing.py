0cbb56da2870ac19a4c96ed24247c0f844b0ba1a
Fabian <accounts@fabianbalsiger.ch>
2018-03-12 16:04:54 +0100

    feat(indexing): add voxel-wise indexing

--- miapy/data/extraction/indexing.py
+++ miapy/data/extraction/indexing.py
@@ -1,6 +1,8 @@
 import abc
 import typing as t
 
+import numpy as np
+
 import miapy.data.indexexpression as expr
 
 
@@ -20,3 +22,22 @@ class SliceIndexing(IndexingStrategy):
     def __call__(self, shape, offset=0) -> t.List[expr.IndexExpression]:
         count = shape[self.slice_axis]
         return [expr.IndexExpression(i, self.slice_axis) for i in range(offset, offset + count)]
+
+
+class VoxelWiseIndexing(IndexingStrategy):
+
+    def __init__(self):
+        self.shape = None
+        self.indexing = None
+
+    def __call__(self, shape, offset=0) -> t.List[expr.IndexExpression]:
+        if self.shape == shape:
+            return self.indexing
+
+        self.shape = shape  # save for later comparison
+        shape_without_voxel = shape[:-1]
+        indices = np.indices(shape_without_voxel)
+        indices = indices.reshape((indices.shape[0], np.prod(indices.shape[1:])))
+        indices = indices.transpose()
+        self.indexing = [expr.IndexExpression(idx.tolist()) for idx in indices]
+        return self.indexing
