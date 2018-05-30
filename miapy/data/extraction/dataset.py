e20252b29a90badca40d9e4cbace4c8e959b1aa3
Fabian <accounts@fabianbalsiger.ch>
2018-05-30 14:16:33 +0200

    feat(ParameterizableDataset): set_indexing_strategy and get_subjects

--- miapy/data/extraction/dataset.py
+++ miapy/data/extraction/dataset.py
@@ -12,26 +12,18 @@ class ParameterizableDataset(data.Dataset):
     def __init__(self, dataset_path: str, indexing_strategy: idx.IndexingStrategy=None, extractor: extr.Extractor=None,
                  transform: tfm.Transform=None, subject_subset: list=None, init_reader_once=True) -> None:
         self.dataset_path = dataset_path
-        if indexing_strategy is None:
-            indexing_strategy = idx.EmptyIndexing()
-        self.indexing_strategy = indexing_strategy
+        self.indexing_strategy = None
         self.extractor = extractor
         self.transform = transform
         self.subject_subset = subject_subset
         self.init_reader_once = init_reader_once
-
         self.indices = []
         self.reader = None
-        with rd.get_reader(dataset_path) as reader:
-            all_subjects = reader.get_subjects()
-            for i, subject in enumerate(reader.get_subject_entries()):
-                if self.subject_subset is None or all_subjects[i] in self.subject_subset:
-                    subject_indices = self.indexing_strategy(reader.get_shape(subject))
-                    subject_and_indices = zip(len(subject_indices) * [i], subject_indices)
-                    self.indices.extend(subject_and_indices)
 
-    def __del__(self):
-        self.close_reader()
+        # init indices
+        if indexing_strategy is None:
+            indexing_strategy = idx.EmptyIndexing()
+        self.set_indexing_strategy(indexing_strategy)
 
     def close_reader(self):
         if self.reader is not None:
@@ -41,8 +33,23 @@ class ParameterizableDataset(data.Dataset):
     def set_extractor(self, extractor: extr.Extractor):
         self.extractor = extractor
 
+    def set_indexing_strategy(self, indexing_strategy: idx.IndexingStrategy):
+        self.indices.clear()
+        self.indexing_strategy = indexing_strategy
+        with rd.get_reader(self.dataset_path) as reader:
+            all_subjects = reader.get_subjects()
+            for i, subject in enumerate(reader.get_subject_entries()):
+                if self.subject_subset is None or all_subjects[i] in self.subject_subset:
+                    subject_indices = self.indexing_strategy(reader.get_shape(subject))
+                    subject_and_indices = zip(len(subject_indices) * [i], subject_indices)
+                    self.indices.extend(subject_and_indices)
+
     def set_transform(self, transform: tfm.Transform):
         self.transform = transform
+
+    def get_subjects(self):
+        with rd.get_reader(self.dataset_path) as reader:
+            return reader.get_subjects()
 
     def direct_extract(self, extractor: extr.Extractor, subject_index: int, index_expr: expr.IndexExpression=None,
                        transform: tfm.Transform=None):
@@ -71,3 +78,6 @@ class ParameterizableDataset(data.Dataset):
     def __getitem__(self, item):
         subject_index, index_expr = self.indices[item]
         return self.direct_extract(self.extractor, subject_index, index_expr, self.transform)
+
+    def __del__(self):
+        self.close_reader()
