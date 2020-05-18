c8f4136177bd26f3053d5cfa45b833a055b65607
alain <alain.benninger@hotmail.com>
2020-05-18 21:47:19 +0200

    refactor(data): name forwarding in the backend package

--- pymia/data/backends/tensorflow/__init__.py
+++ pymia/data/backends/tensorflow/__init__.py
@@ -2,3 +2,5 @@ try:
     import tensorflow
 except ImportError:
     raise ImportError('Module "tensorflow" not found. Install "tensorflow>=2.2.0" to use the tensorflow module.')
+
+from .dataset import get_tf_generator
