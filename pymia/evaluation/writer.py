9a4dcff76aa7882476416a66de93dbc102455f6b
fabianbalsiger <accounts@fabianbalsiger.ch>
2020-05-21 22:27:09 +0200

    refactor(writer): adapt to Result class

--- pymia/evaluation/writer.py
+++ pymia/evaluation/writer.py
@@ -2,28 +2,31 @@ import abc
 import csv
 import logging
 import os
+import typing
 
 import numpy as np
 
+import pymia.evaluation.evaluator as eval_
+
 
 class WriterBase(abc.ABC):
-    """Represents a writer interface to write evaluation results."""
+    """Represents an evaluation results writer interface."""
 
     @abc.abstractmethod
-    def write(self, data: list):
+    def write(self, results: typing.List[eval_.Result]):
         """Writes the evaluation results.
 
         Args:
-            data (list): The evaluation data.
+            results (list of eval_.Result): The evaluation results.
         """
         raise NotImplementedError
 
 
 class CSVWriter(WriterBase):
-    """Represents a CSV (comma-separated values) evaluator writer."""
+    """Represents a CSV file evaluation results writer."""
 
-    def __init__(self, path: str, delimiter: str=';'):
-        """Initializes a new instance of the CSVEvaluatorWriter class.
+    def __init__(self, path: str, delimiter: str = ';'):
+        """Initializes a new instance of the CSVWriter class.
 
         Args:
             path (str): The CSV file path.
@@ -37,24 +40,21 @@ class CSVWriter(WriterBase):
         if not self.path.lower().endswith('.csv'):
             self.path = os.path.join(self.path, '.csv')
 
-    def write(self, data: list):
-        """Writes the evaluation results.
+    def write(self, results: typing.List[eval_.Result]):
+        """Writes the evaluation results to a CSV file.
 
         Args:
-            data (list): The evaluation data.
+            results (list of eval_.Result): The evaluation results.
         """
 
-        # get unique labels
-        labels = []
-
-        # get unique metrics
-        metrics = []
-
-        # get unique ids
-        ids = []
+        # get unique ids, labels, and metrics
+        ids = sorted({result.id_ for result in results})
+        labels = sorted({result.label for result in results})
+        metrics = sorted({result.metric for result in results})
 
         with open(self.path, 'w', newline='') as file:  # creates (and overrides an existing) file
             writer = csv.writer(file, delimiter=self.delimiter)
+
             # write header
             writer.writerow(['SUBJECT', 'LABEL'] + metrics)
 
@@ -62,15 +62,19 @@ class CSVWriter(WriterBase):
                 for label in labels:
                     row = [id_, label]
                     for metric in metrics:
-                        row += [1., ]
+                        # search for result
+                        value = next(
+                            (r.value for r in results if r.id_ == id_ and r.label == label and r.metric == metric),
+                            None)
+                        row.append(value if value else 'n/a')
                     writer.writerow(row)
 
 
 class ConsoleWriter(WriterBase):
-    """Represents a console evaluator writer."""
+    """Represents a console evaluation results writer."""
 
     def __init__(self, precision: int = 3, use_logging: bool = False):
-        """Initializes a new instance of the ConsoleEvaluatorWriter class.
+        """Initializes a new instance of the ConsoleWriter class.
 
         Args:
             precision (int): The float precision.
@@ -81,27 +85,43 @@ class ConsoleWriter(WriterBase):
         self.precision = precision
         self.use_logging = use_logging
 
-    def write(self, data: list):
+    def write(self, results: typing.List[eval_.Result]):
         """Writes the evaluation results.
 
         Args:
-            data (list of list): The evaluation data,
-                e.g. [['PATIENT1', 'BACKGROUND', 0.90], ['PATIENT1', 'TUMOR', '0.62']]
+            results (list of eval_.Result): The evaluation results.
         """
 
-        # format all floats with given precision to str
-        out_as_string = [self.header]
-        for line in data:
-            out_as_string.append([val if isinstance(val, str) else "{0:.{1}f}".format(val, self.precision)
-                                  for val in line])
+        # get unique ids, labels, and metrics
+        ids = sorted({result.id_ for result in results})
+        labels = sorted({result.label for result in results})
+        metrics = sorted({result.metric for result in results})
+
+        # header
+        out_as_string = [['SUBJECT', 'LABEL'] + metrics]
+
+        for id_ in ids:
+            for label in labels:
+                row = [id_, label]
+                for metric in metrics:
+                    # search for result
+                    value = next(
+                        (r.value for r in results if r.id_ == id_ and r.label == label and r.metric == metric),
+                        None)
+                    if value:
+                        # format float with given precision
+                        row.append(value if isinstance(value, str) else f'{value:.{self.precision}f}')
+                    else:
+                        row.append('n/a')
+                out_as_string.append(row)
 
         # determine length of each column for output alignment
         lengths = np.array([list(map(len, row)) for row in out_as_string])
-        lengths = lengths.max(0)
+        lengths = np.max(lengths, axis=0)
         lengths += (len(lengths) - 1) * [2] + [0, ]  # append two spaces except for last column
 
-        # format for output alignment
-        out = [['{0:<{1}}'.format(val, lengths[idx]) for idx, val in enumerate(line)] for line in out_as_string]
+        # format with spaces for output alignment
+        out = [[f'{val:<{lengths[idx]}}' for idx, val in enumerate(line)] for line in out_as_string]
 
         if self.use_logging:
             logging.info('\n'.join(''.join(line) for line in out))
