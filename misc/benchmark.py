b503e60e53f6c4eaaad6aaae3a672ce548c6e17c
alain <alain.benninger@hotmail.com>
2020-06-22 20:46:37 +0200

    feat(misc): add benchmark w/ numpy

--- misc/benchmark.py
+++ misc/benchmark.py
@@ -1,20 +1,24 @@
 import time
 import csv
 
+import numpy as np
+
 import pymia.data.definition as defs
 import pymia.data.extraction as extr
 
 
 def main():
     hdf_file = '../examples/example-data/example-dataset.h5'
-    file_root = '../examples/example-data-uncompressed'
+    file_root = '../examples/example-data'
+    file_un_root = '../examples/example-data-uncompressed'
+    file_np_root = '../examples/example-data-numpy'
     repetitions = 5
 
     results = {}
     repetitions = list(range(repetitions))
     for r in repetitions:
         print(r)
-        for name, (extractor, indexing_strategy) in get_pairs(file_root).items():
+        for name, (extractor, indexing_strategy) in get_pairs(file_root, file_un_root, file_np_root).items():
             dataset = extr.PymiaDatasource(hdf_file, indexing_strategy, extractor, None)
 
             start = time.time()
@@ -24,7 +28,7 @@ def main():
 
             results.setdefault(name, []).append(end - start)
 
-    with open('benchmark_results.csv', 'w', newline='') as f:
+    with open('benchmark_results_w_numpy.csv', 'w', newline='') as f:
         names = ['repetition'] + list(results.keys())
         writer = csv.DictWriter(f, names)
         writer.writeheader()
@@ -34,9 +38,14 @@ def main():
             writer.writerow(row)
 
 
-def get_pairs(uncompressed_file_root):
+def np_read(file_path, cat):
+    return np.load(file_path + '.npy')
+
+
+def get_pairs(file_root, uncompressed_file_root, numpy_file_root):
     data_extractor = extr.DataExtractor(categories=(defs.KEY_IMAGES,))
-    file_extractor = extr.FilesystemDataExtractor(categories=(defs.KEY_IMAGES,))
+    file_extractor = extr.FilesystemDataExtractor(categories=(defs.KEY_IMAGES,), override_file_root=file_root)
+    file_np_extractor = extr.FilesystemDataExtractor(categories=(defs.KEY_IMAGES,), override_file_root=numpy_file_root, load_fn=np_read)
     file_un_extractor = extr.FilesystemDataExtractor(categories=(defs.KEY_IMAGES,), override_file_root=uncompressed_file_root)
 
     slices = extr.SliceIndexing()
@@ -49,12 +58,15 @@ def get_pairs(uncompressed_file_root):
         'dataset-slice': (data_extractor, slices),
         'file-slice': (file_extractor, slices),
         'file-un-slice': (file_un_extractor, slices),
+        'file-np-slice': (file_np_extractor, slices),
         'dataset-patch': (extr.PadDataExtractor(padding, data_extractor), patches),
         'file-patch': (extr.PadDataExtractor(padding, file_extractor), patches),
         'file-un-patch': (extr.PadDataExtractor(padding, file_un_extractor), patches),
+        'file-np-patch': (extr.PadDataExtractor(padding, file_np_extractor), patches),
         'dataset-full': (data_extractor, full),
         'file-full': (file_extractor, full),
         'file-un-full': (file_un_extractor, full),
+        'file-np-full': (file_np_extractor, full),
     }
 
 
