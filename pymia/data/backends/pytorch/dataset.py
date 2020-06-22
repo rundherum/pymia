760b8812047aeb158622c15073f1e5434f5d2d9e
alain <alain.benninger@hotmail.com>
2020-06-22 14:59:08 +0200

    refactor(data): remove unused pytorch dataset

--- pymia/data/backends/pytorch/dataset.py
+++ pymia/data/backends/pytorch/dataset.py
@@ -1,24 +1,6 @@
 import torch.utils.data.dataset as torch_data
 
 import pymia.data.extraction as extr
-import pymia.data.transformation as tfm
-
-
-class PymiaTorchDataset(extr.PymiaDatasource, torch_data.Dataset):
-
-    def __init__(self, dataset_path: str,
-                 indexing_strategy: extr.IndexingStrategy = None,
-                 extractor: extr.Extractor = None,
-                 transform: tfm.Transform = None, subject_subset: list = None, init_reader_once=True) -> None:
-        """Represents a `torch.utils.data.Dataset <https://pytorch.org/docs/stable/data.html#torch.utils.data.Dataset>`_
-        wrapper class for :class:`.PymiaDatasource`.
-
-        See :class:`.PymiaDatasource` for the description of the arguments."""
-
-        # calling parent __init__() directly since super().__init__() is calling both parent inits and
-        # raises an error if there are arguments.
-        extr.PymiaDatasource.__init__(self, dataset_path, indexing_strategy, extractor, transform,
-                                      subject_subset, init_reader_once)
 
 
 class PytorchDatasetAdapter(torch_data.Dataset):
