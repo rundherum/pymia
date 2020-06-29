4c32666b3e9b0bef43b224ddd0e56b63489ce9af
fabianbalsiger <accounts@fabianbalsiger.ch>
2020-06-29 13:50:11 +0200

    feat(evaluation/logging): update logging example

--- examples/evaluation/logging_tensorflow.py
+++ examples/evaluation/logging_tensorflow.py
@@ -1,30 +1,24 @@
 import argparse
+import os
 
-# try:
-import torch
-# except ImportError:
-#     raise ImportError('Module "torch" not found. Install "torch" to run this example.')
-
+try:
+    import tensorflow as tf
+except ImportError:
+    raise ImportError('Module "tensorflow" not found. Install "tensorflow" to run this example.')
 
 import numpy as np
 import pymia.data.assembler as assm
-import pymia.data.backends.pytorch as pymia_torch
-import pymia.data.conversion as conv
+import pymia.data.backends.tensorflow as pymia_tf
 import pymia.data.definition as defs
 import pymia.data.extraction as extr
-import pymia.data.transformation as tfm
 import pymia.evaluation.metric as metric
 import pymia.evaluation.evaluator as eval_
 import pymia.evaluation.writer as writer
-import torch.nn as nn
-import torch.utils.data as torch_data
-import torch.utils.tensorboard as tensorboard
 
 
 def main(hdf_file: str, log_dir: str):
     # initialize the evaluator with the metrics and the labels to evaluate
-    metrics = [metric.DiceCoefficient(), metric.HausdorffDistance(percentile=95, metric='HDRFDST95'),
-               metric.VolumeSimilarity()]
+    metrics = [metric.DiceCoefficient()]
     labels = {1: 'WHITEMATTER',
               2: 'GREYMATTER',
               5: 'THALAMUS'
@@ -36,13 +30,16 @@ def main(hdf_file: str, log_dir: str):
     statistics_aggregator = writer.StatisticsAggregator(functions=functions)
 
     # initialize TensorBoard writer
-    tb = tensorboard.SummaryWriter(log_dir)
+    # tb = tensorboard.SummaryWriter(os.path.join(log_dir, 'logging-example-torch'))
+    tb = tf.summary.create_file_writer(os.path.join(log_dir, 'logging-example-tensorflow'))
 
     # initialize the data handling
-    transform = tfm.Permute(permutation=(2, 0, 1), entries=(defs.KEY_IMAGES,))
-    dataset = extr.PymiaDatasource(hdf_file, extr.SliceIndexing(), extr.DataExtractor(categories=(defs.KEY_IMAGES,)), transform)
-    pytorch_dataset = pymia_torch.PytorchDatasetAdapter(dataset)
-    loader = torch_data.dataloader.DataLoader(pytorch_dataset, batch_size=2, shuffle=False)
+    dataset = extr.PymiaDatasource(hdf_file, extr.SliceIndexing(), extr.DataExtractor(categories=(defs.KEY_IMAGES,)))
+    gen_fn = pymia_tf.get_tf_generator(dataset)
+    tf_dataset = tf.data.Dataset.from_generator(generator=gen_fn,
+                                                output_types={defs.KEY_IMAGES: tf.float32,
+                                                              defs.KEY_SAMPLE_INDEX: tf.int64})
+    loader = tf_dataset.batch(100)
 
     assembler = assm.SubjectAssembler(dataset)
     direct_extractor = extr.ComposeExtractor([
@@ -52,49 +49,48 @@ def main(hdf_file: str, log_dir: str):
     ])
 
     # initialize a dummy network, which returns a random prediction
-    class DummyNetwork(nn.Module):
+    class DummyNetwork(tf.keras.Model):
 
-        def forward(self, x):
-            return torch.randint(0, 5, (*x.size()[:-1], 1))
+        def call(self, inputs):
+            return tf.random.uniform((*inputs.shape[:-1], 1), 0, 6, dtype=tf.int32)
 
     dummy_network = DummyNetwork()
-    torch.manual_seed(0)  # set seed for reproducibility
+    tf.random.set_seed(0)  # set seed for reproducibility
 
-    nb_batches = len(loader)
+    nb_batches = len(dataset) // 2
 
-    for epoch in range(10):
+    epochs = 10
+    for epoch in range(epochs):
+        print(f'Epoch {epoch + 1}/{epochs}')
         for i, batch in enumerate(loader):
             # get the data from batch and predict
             x, sample_indices = batch[defs.KEY_IMAGES], batch[defs.KEY_SAMPLE_INDEX]
             prediction = dummy_network(x)
 
-            # translate the prediction to numpy and back to (B)HWC (channel last)
-            numpy_prediction = prediction.numpy().transpose((0, 2, 3, 1))
+            # translate the prediction to numpy
+            numpy_prediction = prediction.numpy()
 
             # add the batch prediction to the assembler
             is_last = i == nb_batches - 1
             assembler.add_batch(numpy_prediction, sample_indices.numpy(), is_last)
 
-            # Process the subjects/images that are fully assembled
+            # process the subjects/images that are fully assembled
             for subject_index in assembler.subjects_ready:
                 subject_prediction = assembler.get_assembled_subject(subject_index)
 
-                # Extract the target and image properties via direct extract
+                # extract the target and image properties via direct extract
                 direct_sample = dataset.direct_extract(direct_extractor, subject_index)
                 reference, image_properties = direct_sample[defs.KEY_LABELS], direct_sample[defs.KEY_PROPERTIES]
 
-                # convert prediction and reference back to SimpleITK images, as we want to use the physical spacing for the Hausdorff distance metric
-                prediction_image = conv.NumpySimpleITKImageBridge.convert(subject_prediction, image_properties)
-                reference_image = conv.NumpySimpleITKImageBridge.convert(reference, image_properties)
-
                 # evaluate the prediction against the reference
-                evaluator.evaluate(prediction_image, reference_image, direct_sample[defs.KEY_SUBJECT])
+                evaluator.evaluate(subject_prediction[..., 0], reference[..., 0], direct_sample[defs.KEY_SUBJECT])
 
         # calculate mean and standard deviation of each metric
         results = statistics_aggregator.calculate(evaluator.results)
         # log to TensorBoard into category train
         for result in results:
-            tb.add_scalar(f'train/{result.metric}-{result.id_}', result.value, epoch)
+            with tb.as_default():
+                tf.summary.scalar(f'train/{result.metric}-{result.id_}', result.value, epoch)
 
         # clear results such that the evaluator is ready for the next evaluation
         evaluator.clear()
