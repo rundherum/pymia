da5563a6cd55653c7d6162d0e0c3341f6991da4e
fabianbalsiger <accounts@fabianbalsiger.ch>
2020-07-06 23:06:49 +0200

    refactor(plot_task): improve figure

--- misc/plot_tasks.py
+++ misc/plot_tasks.py
@@ -4,9 +4,9 @@ import matplotlib.pyplot as plt
 import matplotlib.cm as cm
 
 
-task_names = {'slice': 'Slice-wise', 'patch': 'Patch-wise\n(84x84x84 with overlap)', 'full': 'Full image'}
-method_names = {'dataset': 'PymiaDatasource', 'file-standard': 'MetaImage (standard)',
-                'file-uncompressed': 'MetaImage (uncompressed)', 'file-numpy': 'Raw NumPy'}
+task_names = {'slice': '2-D image slice', 'patch': '3-D patch\n(84 x 84 x 84)', 'full': '3-D image'}
+method_names = {'dataset': 'HDF5 dataset', 'file-standard': 'MetaImage\n(compressed)',
+                'file-uncompressed': 'MetaImage\n(uncompressed)', 'file-numpy': 'NumPy'}
 
 
 def main():
@@ -21,29 +21,35 @@ def main():
 
     mean_duration = mean_duration.unstack(0)
     std_duration = std_duration.unstack(0)
+    fontsize = 9.5
+    with plt.rc_context({'font.weight': 'bold', 'font.size': fontsize, 'mathtext.default': 'regular'}):
+        bar_width = 0.2
+        x = np.arange(len(mean_duration.columns))
+        methods = mean_duration.index.to_list()
 
-    # with plt.rc_context({'font.weight': 'bold', 'font.size': 12, 'mathtext.default': 'regular'}):
-    bar_width = 0.2
-    x = np.arange(len(mean_duration.columns))
-    methods = mean_duration.index.to_list()
+        colors = cm.get_cmap('viridis')(np.linspace(0, 0.9, len(methods)))
 
-    colors = cm.get_cmap('viridis')(np.linspace(0, 0.9, len(methods)))
+        fig, ax = plt.subplots()
+        ordered_methods = [m for m in method_names if m in methods]
+        for i, method in enumerate(ordered_methods):
+            ax.bar(x + i*bar_width, mean_duration.loc[method],
+                   label=method_names[method], yerr=std_duration.loc[method], width=bar_width,
+                   color=colors[i])
 
-    fig, ax = plt.subplots()
-    ordered_methods = [m for m in method_names if m in methods]
-    for i, method in enumerate(ordered_methods):
-        ax.bar(x + i*bar_width, mean_duration.loc[method],
-               label=method_names[method], yerr=std_duration.loc[method], width=bar_width,
-               color=colors[i])
+        ax.set_xticks(x + (len(methods) - 1) * (bar_width / 2))
+        ax.set_xticklabels(task_names[t] for t in mean_duration.columns.to_list())
 
-    ax.set_xticks(x + (len(methods) - 1) * (bar_width / 2))
-    ax.set_xticklabels(task_names[t] for t in mean_duration.columns.to_list())
+        ax.set_ylabel('Time (s)', fontweight='bold', fontsize=fontsize)
+        ax.set_xlabel('Loading variant', fontweight='bold', fontsize=fontsize)
 
-    ax.set_ylabel('Time (s)')
-    # ax.set_yscale('log')
+        ax.spines['top'].set_visible(False)
+        ax.spines['right'].set_visible(False)
 
-    ax.legend()
-    plt.show()
+        ax.legend(frameon=False, loc='upper center', ncol=len(ordered_methods), bbox_to_anchor=(0.5, 1.15))
+
+        fig.tight_layout()
+        plt.savefig('./fig5.pdf')
+        plt.show()
 
 
 if __name__ == '__main__':
