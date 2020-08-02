e160885b53cafd1280e4fc7bcb4855fa399c5330
fabianbalsiger <accounts@fabianbalsiger.ch>
2020-08-02 21:04:27 +0200

    feat(all): bump version to 0.3.1

--- pymia/__version__.py
+++ pymia/__version__.py
@@ -1,7 +1,7 @@
 __title__ = 'pymia'
 __description__ = 'A Python package for data handling and evaluation in deep learning-based medical image analysis.'
 __url__ = 'https://pymia.readthedocs.io/'
-__version__ = '0.3.0'
+__version__ = '0.3.1'
 __author__ = 'Fabian Balsiger and Alain Jungo'
 __author_email__ = 'fabian.balsiger@artorg.unibe.ch'
 __license__ = 'Apache 2.0'
