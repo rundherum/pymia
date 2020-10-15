fd1ee1757ac23684a73a8f037e43a389ccae9db0
fabianbalsiger <accounts@fabianbalsiger.ch>
2020-10-15 15:59:55 +0200

    docs(augmentation): enhance documentation

--- pymia/data/augmentation.py
+++ pymia/data/augmentation.py
@@ -3,16 +3,18 @@
 The data augmentation bases on the transformation concept (see :class:`pymia.data.transformation.Transform`)
 and can easily be incorporated into the data loading process.
 
+See Also:
+    The pymia documentation features an `example for augmentation <https://pymia.readthedocs.io/en/latest/examples.augmentation.basic.html>`_,
+    which shows how to apply data augmentation in conjunction with the :mod:`pymia.data` package.
+    Besides transformations from the :mod:`pymia.data.augmentation` module, transformations from the Python packages `batchgenerators <https://github.com/MIC-DKFZ/batchgenerators>`_ and `TorchIO <https://github.com/fepegar/torchio/>`_ are integrated.
+
 Warnings:
-    The augmentation relies on the random number generator of `numpy`. If you want to obtain reproducible result,
+    The augmentation relies on the random number generator of ``numpy``. If you want to obtain reproducible result,
     set numpy's seed prior to executing any augmentation:
 
     >>> import numpy as np
     >>> your_seed = 0
     >>> np.random.seed(your_seed)
-
-See Also:
-    https://github.com/MIC-DKFZ/batchgenerators
 """
 import typing
 import warnings
